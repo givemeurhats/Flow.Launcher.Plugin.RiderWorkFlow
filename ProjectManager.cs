2fa7c9ae31139a631395a18387cc07938e76c22c
givemeurhats <github-givemeurhats.discuss887@passmail.net>
2025-08-11 21:28:00 +0200

    - fix for paths in recentSolutions.xml with $USER_HOME$ - small optimizations on string comparisons

--- ProjectManager.cs
+++ ProjectManager.cs
@@ -8,13 +8,14 @@ public class ProjectManager
 {
     private const string IcoPath = "Images/Icon.png";
     private const string IDEName = "rider";
+    private const string UserHomePlaceholder = "$USER_HOME$";
     private readonly string _optionPath;
 
     public ProjectManager()
     {
         _optionPath = GetOptionPath();
     }
-    
+
     public List<Result> GetResultProjects(string projectName)
     {
         return GetProjects(projectName)
@@ -26,18 +27,30 @@ public class ProjectManager
                 Action = (c) => OpenProject(e.ProjectPath)
             }).ToList();
     }
-    
+
     private List<ProjectModel> GetProjects(string projectName)
     {
         var xmlContent = File.ReadAllText(_optionPath);
         var xmlDoc = XDocument.Parse(xmlContent);
         var names = xmlDoc.Descendants("entry")
-            .Select(e => new ProjectModel{ ProjectPath = e.Attribute("key")!.Value })
-            .Where(e => e.Name.ToLower().Contains(projectName.ToLower())).ToList();
-        
+            .Select(e => new ProjectModel { ProjectPath = FixPathForWindows(ReplaceUserHome(e.Attribute("key")!.Value)) })
+            .Where(e => e.Name.Contains(projectName, StringComparison.CurrentCultureIgnoreCase)).ToList();
+
         return names;
     }
-    
+
+    private static string ReplaceUserHome(string path)
+    {
+        if (string.IsNullOrEmpty(path)) return path;
+        return path.Replace(UserHomePlaceholder, Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+    }
+
+    private static string FixPathForWindows(string path)
+    {
+        if (string.IsNullOrEmpty(path)) return path;
+        return path.Replace("/", "\\");
+    }
+
     private static bool OpenProject(string solutionPath)
     {
         var processStartInfo = new ProcessStartInfo
@@ -61,11 +74,11 @@ public class ProjectManager
     {
         var applicationDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\JetBrains\";
         const string optionsFilePath = @"\options\recentSolutions.xml";
-        
+
         var riderVersion = Directory.GetDirectories(applicationDataFolder)
             .Select(Path.GetFileName)
-            .FirstOrDefault(e => e != null && e.ToLower().Contains(IDEName));
-        
+            .FirstOrDefault(e => e != null && e.Contains(IDEName, StringComparison.CurrentCultureIgnoreCase));
+
         return applicationDataFolder + riderVersion + optionsFilePath;
     }
 }
